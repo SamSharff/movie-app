a77a1a2f1bffbff29614d5f8dcae5d6559842104
Sam Sharff <samanthaleec@gmail.com>
2022-03-12 13:18:26 -0500

    adds one_movie action to movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,12 @@ class MoviesController < ApplicationController
     render json: movies.as_json
   end
 
+  # Note on one_movie (below): I was playing around with find_by and calling data. I wanted to see if movie.title (line 10) would know the movie I was calling on line 9).
+  def one_movie 
+    movie = Movie.find_by(year: 1992) 
+    render json: {message: "You chose #{movie.title}."}
+  end 
+
 end
   # def all_movies
   #   movie = Movie.all
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
 get "/actor", controller: "actors", action: "one_actor"
 get "/all_movies", controller: "movies", action: "all_movies"
+get "/one_movie", controller: "movies", action: "one_movie"
+
+
 end
