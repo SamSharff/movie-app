2df3762e5ba6d0e342bc9d377fde58113f6f5ad2
Sam Sharff <samanthaleec@gmail.com>
2022-03-12 12:41:18 -0500

    adds updated, correct actor model. corrects 'actor' controller to 'actors'.

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,6 @@
-class ActorController < ApplicationController
-  def all_actors
-    actor = Actors.find_by(id: 4)
+class ActorsController < ApplicationController
+  def one_actor
+    actor = Actor.find_by(id: 4)
     render json: actor.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
-get "/all_actors", controller: "actors", action: "all_actors"
+  get "/actor", controller: "actors", action: "one_actor"
 
-get "/all_movies", controller: "movies", action: "all_movies"
 
-get "/one_movie", controller: "movies", action: "one_movie"
+
+
+# get "/all_actors", controller: "actors", action: "all_actors"
+
+# get "/all_movies", controller: "movies", action: "all_movies"
+
+# get "/one_movie", controller: "movies", action: "one_movie"
 end
