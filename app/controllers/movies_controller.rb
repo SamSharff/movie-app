18a3473c0f27609716dd05b9e764790371a5835a
Sam Sharff <samanthaleec@gmail.com>
2022-03-12 13:48:09 -0500

    adds attempt at .includes query.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,13 +10,13 @@ class MoviesController < ApplicationController
     render json: {message: "You chose #{movie.title}."}
   end 
 
-end
-  # def all_movies
-  #   movie = Movie.all
-  #   render :json movie.as_json
-  # end
+  def includes # I can't get this to work, but here are my two attempts
+    
+    #1) movie = Movie.includes(:title, :year)
+    # render json: movie.as_json
 
-  # def one_movie
-  #   movie = Movie.find_by(id: 5)
-  #   render :json movie.as_json
-  # end
+    #2) Movie.includes(:title, :year)
+    # render json: Movie.as_json
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 get "/actor", controller: "actors", action: "one_actor"
 get "/all_movies", controller: "movies", action: "all_movies"
 get "/one_movie", controller: "movies", action: "one_movie"
+get "/includes", controller: "movies", action: "includes"
 
 
 end
