d94fad1e287b9955e9e891c8156aca19df716962
Sam Sharff <samanthaleec@gmail.com>
2022-03-14 10:33:53 -0400

    adds route and http (Post, Rest Client) request for body parameters request, one actor.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ get "/all_movies", controller: "movies", action: "all_movies"
 get "/one_movie", controller: "movies", action: "one_movie"
 get "/includes", controller: "movies", action: "includes"
 get "/actor_query_param", controller: "actors", action: "actor_query_param"
+get "/actor_query_param/:id", controller: "actors", action: "actor_query_param"
+post "/actor_query_param", controller: "actors", action: "actor_query_param"
 
 end
