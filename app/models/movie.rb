be46fa061ae597fed134e1d99d2000788ffcbe47
Sam Sharff <samanthaleec@gmail.com>
2022-03-19 09:18:44 -0400

    adds validations to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :year, length: { is: 4 }
+  validates :plot, presence: true
+  validates :director, presence: true  
+
 end
