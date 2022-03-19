cfc23e96d90f3575e5f015a052744ad47987eb52
Sam Sharff <samanthaleec@gmail.com>
2022-03-19 18:31:07 -0400

    adds new migration (movie_id to to Actors table), adds movies to Movies database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,9 @@
 
 # movie = Movie.new ({title: "Harry Potter and the Half-Blood Prince", year: 2009, plot: "As Harry Potter begins his sixth year at Hogwarts, he discovers an old book marked as 'the property of the Half-Blood Prince' and begins to learn more about Lord Voldemort's dark past."})
 # movie.save
+
+# movie = Movie.new ({title: "Wine Country", year: 2019, plot: "During a vacation to Napa Valley, a group of long time friends reunite and revisit past choices in this hilarious and heartfelt comedy from director Amy Poehler.", director: "Amy Poehler"})
+# movie.save
+
+# movie = Movie.new ({title: "The Power of the Dog", year: 2021, plot: "Charismatic rancher Phil Burbank inspires fear and awe in those around him. When his brother brings home a new wife and her son, Phil torments them until he finds himself exposed to the possibility of love.", director: "Jane Campion"})
+# movie.save
