c361ace1cb8320db798d7cdd7f77555b41867532
Sam Sharff <samanthaleec@gmail.com>
2022-03-21 11:43:49 -0400

    adds updates to index and show views

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,11 +1,11 @@
 class ActorsController < ApplicationController
   def index
-    actors = Actor.all
-    render json: actors.as_json
+    @actors = Actor.all
+    render json: @actors.as_json
   end
 
   def create
-    actor = Actor.new(
+    @actor = Actor.new(
     first_name: params["first_name"],
     last_name: params["last_name"],
     known_for: params["known_for"],
@@ -14,40 +14,40 @@ class ActorsController < ApplicationController
     movie_id: params["movie_id"]
 
     )
-    if actor.save 
-      render json: actor.as_json
+    if @actor.save 
+      render json: @actor.as_json
     else
-      render json: {errors: actor.errors.full_messages}, status: 422
+      render json: {errors: @actor.errors.full_messages}, status: 422
     end
   end
 
   def show
     actor_id = params["id"]
-    actor = Actor.find_by(id: actor_id)
-    render json: actor.as_json    
+    @actor = Actor.find_by(id: actor_id)
+    render json: @actor.as_json    
   end
 
   def update
     actor_id = params["id"]
-    actor = Actor.find_by(id: actor_id)
+    @actor = Actor.find_by(id: actor_id)
 
-    actor.first_name = params["first_name"] || actor.first_name
-    actor.last_name = params["last_name"] || actor.last_name
-    actor.known_for = params["known_for"] || actor.known_for
-    actor.gender = params["gender"] || actor.gender
-    actor.age = params["age"] || actor.age
-    actor.movie_id = params["movie_id"] || actor.movie_id
+    @actor.first_name = params["first_name"] || @actor.first_name
+    @actor.last_name = params["last_name"] || @actor.last_name
+    @actor.known_for = params["known_for"] || @actor.known_for
+    @actor.gender = params["gender"] || @actor.gender
+    @actor.age = params["age"] || @actor.age
+    @actor.movie_id = params["movie_id"] || @actor.movie_id
 
-    if actor.save 
-      render json: actor.as_json 
+    if @actor.save 
+      render json: @actor.as_json 
     else
-      render json: {errors: actor.errors.full_messages}, status: 422
+      render json: {errors: @actor.errors.full_messages}, status: 422
     end
   end
   
   def destroy
     actor_id = params["id"]
-    actor = Actor.find_by(id: actor_id)
+    @actor = Actor.find_by(id: actor_id)
 
     actor.destroy
     render json: {message: "CRUSH! BOOM! SLAM! Destruction complete!"}.as_json
