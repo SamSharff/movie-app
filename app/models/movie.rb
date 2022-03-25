d310edb5c7d0e37611c20cda9094eba9dc202fda
Sam Sharff <samanthaleec@gmail.com>
2022-03-25 09:40:41 -0400

    adds belongs_to and has_many relationships to movie_genre.rb, movie.rb, and genre.rb models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ApplicationRecord
-  has_many :actors
-  
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+  has_many :actors 
+
   # validates :title, presence: true
   # validates :year, length: { is: 4 }
   # validates :plot, presence: true
