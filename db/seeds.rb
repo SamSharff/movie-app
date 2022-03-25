a568d54cf1003a35dbef8c2e38a9f07bb78bcedb
Sam Sharff <samanthaleec@gmail.com>
2022-03-25 09:09:49 -0400

    adds MovieGenre instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,39 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+# moviegenre = MovieGenre.new({genre_id: 2, movie_id: 1})
+# moviegenre.save
 
-genre = Genre.new({name: "Thriller"})
-genre.save
+# moviegenre = MovieGenre.new({genre_id: 4, movie_id: 2})
+# moviegenre.save
 
-genre = Genre.new({name: "Comedy"})
-genre.save
+# moviegenre = MovieGenre.new({genre_id: 2, movie_id: 2})
+# moviegenre.save
 
-genre = Genre.new({name: "Fantasy"})
-genre.save
+# moviegenre = MovieGenre.new({genre_id: 5, movie_id: 3})
+# moviegenre.save
 
-genre = Genre.new({name: "Sports"})
-genre.save
+# moviegenre = MovieGenre.new({genre_id: 3, movie_id: 6})
+# moviegenre.save
 
-genre = Genre.new({name: "Drama"})
-genre.save
+# moviegenre = MovieGenre.new({genre_id: 5, movie_id: 6})
+# moviegenre.save
+
+
+# genre = Genre.new({name: "Thriller"})
+# genre.save
+
+# genre = Genre.new({name: "Comedy"})
+# genre.save
+
+# genre = Genre.new({name: "Fantasy"})
+# genre.save
+
+# genre = Genre.new({name: "Sports"})
+# genre.save
+
+# genre = Genre.new({name: "Drama"})
+# genre.save
 
 # actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
 # actor.save
