6a69dd00822058cc7a978e7dbda731f89fbf91f4
Sam Sharff <samanthaleec@gmail.com>
2022-03-25 16:07:43 -0400

    adds index/show views and  instance variables to movies controllers for views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,20 +2,21 @@ class MoviesController < ApplicationController
   before_action :authenticate_admin, except: [:index, :show]
   
   def index
-    movies = Movie.all
-    render json: movies.as_json
+    @movies = Movie.all
+    render :index
   end
 
   def create
-    movie = Movie.new(
+    @movie = Movie.new(
       title: params["title"],
       year: params["year"],
       plot: params["plot"],
       director: params["director"],
-    
+      english: params["english"],
+      actors: params["actors"]
       )
-    if movie.save
-      render json: movie.as_json
+    if @movie.save
+      render :show
     else
       render json: {errors: movie.errors.full_messages}, status: 422
     end
@@ -23,21 +24,23 @@ class MoviesController < ApplicationController
 
   def show
     movie_id = params["id"]
-    movie = Movie.find_by(id: movie_id)
-    render json: movie.as_json
+    @movie = Movie.find_by(id: movie_id)
+    render :show
   end
 
   def update
-    movie_id = params["id"]
-    movie = Movie.find_by(id: movie_id)
+    @movie_id = params["id"]
+    @movie = Movie.find_by(id: movie_id)
 
-    movie.title = params["title"] || movie.title
-    movie.year = params["year"] || movie.year
-    movie.plot = params["plot"] || movie.plot
-    movie.director = params["director"] || movie.director
+    @movie.title = params["title"] || movie.title
+    @movie.year = params["year"] || movie.year
+    @movie.plot = params["plot"] || movie.plot
+    @movie.director = params["director"] || movie.director
+    @movie.english = params["english"] || movie.english
+    @movie.actors = params["actors"] || movie.actors
     
-    if movie.save
-      render json: movie.as_json
+    if @movie.save
+      render :show
     else
       render json: {errors: movie.errors.full_messages}, status: 422
     end
